596f8eccd6e27e96ca22da7b90b7572a7e87dedf
g-hi3 <marc.forrer@bluewin.ch>
2024-01-27 15:54:15 +0100

    introduced initial (scuffed) frontend layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,29 @@
 import styles from "./App.module.css";
 
+const onToggleHamburger = (pointerEvent) => {
+    console.log(pointerEvent);
+};
+
 const App = () => {
     return (
         <div class={styles.App}>
             <header class={styles.header}>
-                <p>Hello World</p>
+                <h1>Game Prices</h1>
             </header>
+            <nav class={styles.nav}>
+                <button onClick={onToggleHamburger}>|||</button>
+                <ul>
+                    <li>Games</li>
+                    <li>Stores</li>
+                    <li>Orders</li>
+                </ul>
+            </nav>
+            <main class={styles.main}>
+                <p>Hello World</p>
+            </main>
+            <footer class={styles.footer}>
+                <p>Hi, there are some status infos here.</p>
+            </footer>
         </div>
     );
 };
